ac60fb4a50da11ec751a6e7f7ae06a08c5d4b10e
Josef Kosta <433545@mail.muni.cz>
2016-11-05 15:59:06 +0100

    Adding base bl classes

---
+++ BL/Services/AppService.cs
@@ -0,0 +1,12 @@
+﻿using Riganti.Utils.Infrastructure.Core;
+
+namespace BL.Services
+{
+    /// <summary>
+    /// Base class for all eshop services
+    /// </summary>
+    public abstract class AppService
+    {
+        public IUnitOfWorkProvider UnitOfWorkProvider { get; set; }
+    }
+}
