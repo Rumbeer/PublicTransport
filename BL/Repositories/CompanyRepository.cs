1ecf51609bf9dd558175d317c0f3a7e80f837853
Stefan Bojnak <433823@mail.muni.cz>
2016-11-05 15:04:40 +0100

    added companyrepository

---
+++ BL/Repositories/CompanyRepository.cs
@@ -0,0 +1,12 @@
+﻿using DAL.Entities;
+using Riganti.Utils.Infrastructure.Core;
+using Riganti.Utils.Infrastructure.EntityFramework;
+
+
+namespace BL.Repositories
+{
+    public class CompanyRepository : EntityFrameworkRepository<Company, int>
+    {
+        public CompanyRepository(IUnitOfWorkProvider provider) : base(provider) { }
+    }
+}
